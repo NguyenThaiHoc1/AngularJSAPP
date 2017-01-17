2b85dbfdaaa855475fa9dcbca467ae9e173e07c4
huynhdoquang <huynh.fetel@gmail.com>
2017-01-17 18:29:25 +0700

    edit class_record.sql status

--- Sample data/class_record.sql
+++ Sample data/class_record.sql
@@ -1,5 +1,5 @@
 SELECT * FROM dcc_development.class_record;
-INSERT INTO `dcc_development`.`class_record` (`classId`, `traineeEmail`) VALUES ('1', 'qwe@gmail.com');
-INSERT INTO `dcc_development`.`class_record` (`classId`, `traineeEmail`) VALUES ('2', 'qwe@gmail.com');
-INSERT INTO `dcc_development`.`class_record` (`classId`, `traineeEmail`) VALUES ('3', 'thach@gmail.com');
-INSERT INTO `dcc_development`.`class_record` (`classId`, `traineeEmail`) VALUES ('4', 'thach@gmail.com');
+INSERT INTO `dcc_development`.`class_record` (`status`,`classId`, `traineeEmail`) VALUES ('Enrolled','1', 'qwe@gmail.com');
+INSERT INTO `dcc_development`.`class_record` (`status`,`classId`, `traineeEmail`) VALUES ('Learned','2', 'qwe@gmail.com');
+INSERT INTO `dcc_development`.`class_record` (`status`,`classId`, `traineeEmail`) VALUES ('Enrolled','3', 'thach@gmail.com');
+INSERT INTO `dcc_development`.`class_record` (`status`,`classId`, `traineeEmail`) VALUES ('Learned','4', 'thach@gmail.com');
