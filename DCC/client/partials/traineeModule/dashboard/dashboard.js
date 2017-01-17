37e70bfac44971e1f017e24765c27a2ca8ab876a
huynhdoquang <huynh.fetel@gmail.com>
2017-01-17 16:26:43 +0700

    add backend function for trainee dashboard

--- DCC/client/partials/traineeModule/dashboard/dashboard.js
+++ DCC/client/partials/traineeModule/dashboard/dashboard.js
@@ -19,7 +19,7 @@ myApp.factory('dashboardServices', ['$http', function($http) {
 
     var factoryDefinitions = {
       getMyTraingPrograms: function() {
-        return $http.get('partials/traineeModule/dashboard/mock/myTrainingprogram.json').success(function(data) { return data; });
+        return $http.get('/trainee/dashboard/getTrainingProgram').success(function(data) { return data; });
       },
 	}
 
@@ -34,19 +34,20 @@ myApp.controller('MyCoursesCtrl', ['$scope', 'dashboardServices', function($scop
     $scope.actionOneText = {}; $scope.actionTwoText = {};
     $scope.actionOneText['Enrolled'] = 'Give feedback';
     $scope.actionTwoText['Enrolled'] = 'Re-enroll';
-    $scope.actionOneText['Learned'] = 'View Schedule';    
+    $scope.actionOneText['Learned'] = 'View Schedule';
     $scope.actionTwoText['Learned'] ='Un-enroll';
 
     //get all courses and training programs
     dashboardServices.getMyTraingPrograms().then(function(result){
-        result.data.forEach(traningProgram => {
-            traningProgram.myCourseList.forEach(course => {
+        result.data.data.forEach(traningProgram => {
+            traningProgram.Courses.forEach(course => {
+                course.status = course.Classes[course.Classes.length - 1].ClassRecords[course.Classes.length - 1].status;
                 if (course.status == 'Enrolled') {course.backgroundColor = '#4FC3F7'}
                 else if (course.status == 'Learned') {course.backgroundColor = '#8BC34A'}
                 else {course.backgroundColor = 'black'}
             });
         });
-		$scope.myTrainingProgramList = result.data;
+		$scope.myTrainingProgramList = result.data.data;
 
 	});
 }]);
--- DCC/server/routes/trainee/dashboard.js
+++ DCC/server/routes/trainee/dashboard.js
@@ -3,15 +3,33 @@ var models = require('../../models');
 var log = require('../../../config/logConfig');
 
 router.get('/getTrainingProgram', function(req, res){
-    var query = { include: [ models.Course ]};
-    models.TrainingProgram.findAll(query).then(
-        function(trainingProgram) {
-            var datasend = {
-                msg:'send list success',
-                data: trainingProgram
-            };
-            res.send(datasend);
-        });
+    var query =
+    {
+        include: [
+            {
+                model: models.Course,
+                include: [
+                    {
+                        model: models.Class,
+                        include: [
+                            {
+                                model: models.ClassRecord,
+                                where: {traineeEmail: req.user.email}
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
+
+    models.TrainingProgram.findAll(query).then(function(trainingProgram) {
+        var datasend = {
+            msg:'send list success',
+            data: trainingProgram
+        };
+        res.send(datasend);
     });
+});
 
 module.exports = router;
