ae37f74319075957783912ad7ce43af73689831c
binhkhang315 <binhkhang315@gmail.com>
2017-01-19 16:29:42 +0700

    edit request_opening.sql Sample data: add requestType column

--- Sample data/request_opening.sql
+++ Sample data/request_opening.sql
@@ -1,4 +1,4 @@
 SELECT * FROM dcc_development.request_opening;
-INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`) VALUES ('qwe@gmail.com', '2');
-INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`) VALUES ('qwe@gmail.com', '5');
-INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`) VALUES ('thach@gmail.com', '6');
+INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('qwe@gmail.com', '2','open');
+INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('qwe@gmail.com', '5','open');
+INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('thach@gmail.com', '6','open');
