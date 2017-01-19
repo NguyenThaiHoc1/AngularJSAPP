9d47fb811478abdd19d8424e16d5de9056d86aca
binhkhang315 <binhkhang315@gmail.com>
2017-01-19 18:34:17 +0700

    edit Sample data: request_opening.sql

--- Sample data/request_opening.sql
+++ Sample data/request_opening.sql
@@ -1,4 +1,4 @@
 SELECT * FROM dcc_development.request_opening;
-INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('qwe@gmail.com', '2','open');
-INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('qwe@gmail.com', '5','open');
-INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('thach@gmail.com', '6','open');
+INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('qwe@gmail.com', '2','register');
+INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('qwe@gmail.com', '5','register');
+INSERT INTO `dcc_development`.`request_opening` (`userEmail`, `courseId`,`requestType`) VALUES ('thach@gmail.com', '6','register');
