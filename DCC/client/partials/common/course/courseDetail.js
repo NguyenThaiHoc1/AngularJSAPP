afebe30a57debb6fab157973f22da86bbde2bbc5
huynhdoquang <huynh.fetel@gmail.com>
2017-02-06 12:01:24 +0700

    edit courseDetail.html and courseDetail.js & route course.js for Class

--- DCC/client/partials/common/course/courseDetail.js
+++ DCC/client/partials/common/course/courseDetail.js
@@ -23,6 +23,9 @@ myApp.factory('courseDetailServices', ['$http', function($http) {
         // sendFeedback: function(req) {
         //     return $http.post('/course/giveFeedback', req).success(function(data) { return data; });
         // },
+        getClassByCourseID: function(courseId){
+            return $http.post('/common/course/getClassByCourseID', {courseId : courseId}).success(function(data) { return data; });
+        }
     }
 
     return factoryDefinitions;
@@ -35,6 +38,11 @@ myApp.controller('courseDetailCtrl', ['$scope', '$rootScope', '$stateParams', 'c
     $scope.courseDetail = {};
     courseDetailServices.getCourseDetailById($stateParams.courseId).then(function(result){
         $scope.courseDetail = result.data.data;
+    });
+
+    $scope.classList = {};
+    courseDetailServices.getClassByCourseID($stateParams.courseId).then(function(result){
+        $scope.classList = result.data.data;
     });
     //
     // $scope.giveFeedback = function(){
@@ -61,4 +69,68 @@ myApp.controller('courseDetailCtrl', ['$scope', '$rootScope', '$stateParams', 'c
         $scope.overStar = value;
     };
 
+}]);
+
+//Add and Edit Class Control
+$scope.showUpdateClassForm = function(course, adminclass){
+    $rootScope.addEditFormIsEditForm =  true;
+    //Class
+    $rootScope.addEditClassFormTitle = 'Edit Class';
+    $rootScope.addEditClassFormAction = 'Update Class';
+    console.log(  adminclass); //debug
+    $rootScope.adminClassModel = {
+        id: adminclass.id,
+        courseId: course.id,
+        location: adminclass.location,
+        //TODO
+        // trainerId: adminclass.trainerId,
+        startTime: adminclass.startTime,
+        duration: adminclass.duration,
+        maxAttendant: adminclass.maxAttendant,
+        note: adminclass.note
+    };
+};
+
+$scope.showDeleteClassForm = function(adminclass){
+    $rootScope.deleteClickId = 3;
+    //Class
+    $rootScope.adminClassModel = {
+        id: adminclass.id,
+    };
+};
+
+myApp.controller('addEditClassCtrl', [ '$scope', '$rootScope','courseManagementServices', function($scope, $rootScope, courseManagementServices, $location) {
+
+    $scope.addEditClassClick = function(){
+        if ($rootScope.addEditFormIsEditForm){
+            //edit class
+            courseManagementServices.updateClass($rootScope.adminClassModel).then(function(result){
+                if (result.data.success){
+                    //Get Training Program
+                    courseManagementServices.getTrainingProgramList().then(function(result) {
+                        $rootScope.adminTrainingProgramList = result.data.trainingProgram;
+                    });
+                    $rootScope.ShowPopupMessage(result.data.msg, "success");
+                    $location.path("#admin_courseManagement");
+                }else{
+                    $rootScope.ShowPopupMessage('Add Class FAIL!',"error");
+                }
+            });
+        }
+        else {
+            //add Class
+            courseManagementServices.addClass($rootScope.adminClassModel).then(function(result) {
+                if (result.data.success){
+                    //GetTrainingProgram
+                    courseManagementServices.getTrainingProgramList().then(function(result) {
+                        $rootScope.adminTrainingProgramList = result.data.trainingProgram;
+                    });
+                    // $location.path("/userProfile");
+                    $rootScope.ShowPopupMessage(result.data.msg, "success");
+                } else {
+                    $rootScope.ShowPopupMessage('Edit Class Info FAIL!', "error");
+                }
+            });
+        }
+    };
 }]);
