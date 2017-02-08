81d7c40578a52355e6c343579c02b4edd6ee29e3
binhkhang315 <binhkhang315@gmail.com>
2017-02-08 14:39:19 +0700

    edit trainingprogram model for testing sonar coverage

--- DCC/server/models/trainingprogram.js
+++ DCC/server/models/trainingprogram.js
@@ -3,20 +3,18 @@ var _trainingprogramModel= require('./DataObjects/trainingProgram');
 var Sequelize = require('sequelize');
 
 module.exports = function(sequelize) {
-  var Trainingprogram = sequelize.define('TrainingProgram', _trainingprogramModel,{
-      classMethods:{
-        getTraining: function(cb)
-        {
-          log.info('/models/course: getCourses() : ');
-          var query =
-          {
-                include: [ models.Course ]
-          };
-          Trainingprogram.findAll(query).then(cb);
+    var Trainingprogram = sequelize.define('TrainingProgram', _trainingprogramModel,{
+        classMethods:{
+            getTraining: function(cb){
+                log.info('/models/course: getCourses() : ');
+                var query = {
+                    include: [ models.Course ]
+                };
+                Trainingprogram.findAll(query).then(cb);
+            }
         },
-      },
-      tableName: 'training_program',
-      timestamps: false
-  });
+        tableName: 'training_program',
+        timestamps: false
+    });
     return Trainingprogram;
 };
