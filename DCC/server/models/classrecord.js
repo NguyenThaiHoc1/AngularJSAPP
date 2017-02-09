eff257da33749ea8928ef9bf00a70b12aa9ac81d
binhkhang315 <binhkhang315@gmail.com>
2017-02-09 16:36:34 +0700

    edit model for sonar test

--- DCC/server/models/classrecord.js
+++ DCC/server/models/classrecord.js
@@ -2,16 +2,6 @@ var _classrecordModel = require('./DataObjects/classRecord');
 module.exports = function(sequelize) {
     var Classrecord = sequelize.define('ClassRecord', _classrecordModel, {
         classMethods: {
-            addTraineeToClass: function(traineeEmail, classId, cb)
-            {
-                var query = {
-                    traineeEmail : traineeEmail,
-                    classId : classId,
-                    status: "Enrolled"
-                };
-                Classrecord.create(query).then(cb);
-            },
-
             unEnrollCourse: function(traineeEmail, classId, cb){
                 var query = {
                     where:
