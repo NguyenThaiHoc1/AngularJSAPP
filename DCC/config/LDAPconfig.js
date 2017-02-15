dad856df861e8e19bf83dcb116565dec6c5d8ac3
binhkhang315 <binhkhang315@gmail.com>
2017-02-15 09:49:04 +0700

    change ip address for ldap: 192.168.122.20 -> 192.168.122.23

--- DCC/config/LDAPconfig.js
+++ DCC/config/LDAPconfig.js
@@ -1,7 +1,7 @@
   var LDAP_PORT = 389;
 module.exports = {
     server: {
-        url: 'ldap://192.168.122.20:' + LDAP_PORT.toString(),
+        url: 'ldap://192.168.122.23:' + LDAP_PORT.toString(),
         bindDn: 'cn=admin,dc=example,dc=com',
         bindCredentials: '123456',
         searchBase: 'dc=example,dc=com',
