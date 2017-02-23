ade30bdbd375f9daa58da2e491c9d5570eb2a9de
Tran Hoang Nam <Tran Hoang Nam>
2017-02-23 11:37:07 +0700

    - Fix User Type null when add new user - Add auto clear fields when submit successfully - Make password not visible when input - Enable password change in user edit page

--- DCC/server/routes/user/userProfile.js
+++ DCC/server/routes/user/userProfile.js
@@ -72,7 +72,8 @@ router.post('/updateUserProfile', function (req, res) {
             status: req.body.status,
             dob: req.body.dob,
             phone: req.body.phone,
-            role: req.body.role
+            role: req.body.role,
+            password: req.body.password
         },
         {
             where: { email: req.body.email }
@@ -112,13 +113,12 @@ router.post('/addUser', function (req, res) {
     models.User.sync({
         force: false
     }).then(function () {
-        console.log(req.body);
         // this function check if the courseName is already existed
         models.User.getUserByEmail(req.body.email, function (result) {
             if (result) {
                 res.send({
                     success: false,
-                    msg: 'Email already existed. Add fail!'
+                    msg: 'Email already existed. Add failed!'
                 });
             } else {
                 models.User.create({
@@ -135,7 +135,7 @@ router.post('/addUser', function (req, res) {
                     isTrainee: true, //default user is a trainee
                     belong2Team: 'Team InNoVa',
                     isExperienced: 0,
-                    courseTypeId: req.body.courseId,
+                    userType: req.body.courseId,
                 }).then(function () {
                     res.send({
                         success: true,
