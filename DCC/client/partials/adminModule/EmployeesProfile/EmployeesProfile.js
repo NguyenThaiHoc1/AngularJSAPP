d6172a5e6ab8701f4028e19f28d5237a3d9109da
Ly Hon Quang <lyhonquang@gmail.com>
2017-02-28 11:55:03 +0700

    [Employees list]Search user by Name, dob, Team, Area and Email

--- DCC/client/partials/adminModule/EmployeesProfile/EmployeesProfile.js
+++ DCC/client/partials/adminModule/EmployeesProfile/EmployeesProfile.js
@@ -32,7 +32,9 @@ myApp.controller('getProfilesController', ['$scope','$sce', 'EmployeesProfileSer
         $scope.UsersList.forEach(user => {
             if ((user.username.toUpperCase().indexOf(userSearchKey.toUpperCase()) !== -1) ||
                 (user.belong2Team.toUpperCase().indexOf(userSearchKey.toUpperCase()) !== -1) ||
-                (user.userType.toUpperCase().indexOf(userSearchKey.toUpperCase()) !== -1))
+                (user.userType.toUpperCase().indexOf(userSearchKey.toUpperCase()) !== -1) ||
+                (user.dob.toUpperCase().indexOf(userSearchKey.toUpperCase()) !== -1) ||
+                (user.email.toUpperCase().indexOf(userSearchKey.toUpperCase()) !== -1))
                     SearchResult.push(user);
         });
         $scope.UsersListSearchResult = SearchResult;
