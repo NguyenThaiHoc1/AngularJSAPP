d83212c6b5af1d32d997b49e1913be1ef7c3590a
Nguyen Thi Lan Anh <lananhnt@outlook.com.vn>
2017-03-02 18:12:52 +0700

    [Test] Test case fixed

--- DCC/test/routes/trainee/courseRegister.js
+++ DCC/test/routes/trainee/courseRegister.js
@@ -113,10 +113,10 @@ describe('<Unit test for trainee-courseRegister>', function () {
                 .post('/trainee/courseRegister/sendRegisterRequest');
             req.cookies = Cookies;
             req.set('Accept', 'application/json')
-                .send({ userId: 1, courseId: 13 })
+                .send({ userId: 1, courseId: 36 })
                 .end(function (err, res) {
                     assert.equal(res.body.success, true);
-                    models.RequestOpening.destroy({ where: { userId: 1, courseId: 13 } });
+                    models.RequestOpening.destroy({ where: { userId: 1, courseId: 36 } });
                     if (err) return done(err);
                     done();
                 });
