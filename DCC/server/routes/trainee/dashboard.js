188daa20ade8226fa5bd2befa2b7b2f9aaa38895
Tran Hoang Nam <Tran Hoang Nam>
2017-03-09 09:17:01 +0700

    [ Test Case ] Add test case for checkpassword route

--- DCC/server/routes/trainee/dashboard.js
+++ DCC/server/routes/trainee/dashboard.js
@@ -30,23 +30,23 @@ router.post('/getTrainingProgramByTPType', function (req, res) {
     models.TrainingProgram.findAll(query).then(function (trainingPrograms) {
         var resData = [];
         trainingPrograms.forEach(trainingProgram => {
-            if (trainingProgram.dataValues.name === req.body.userType || trainingProgram.dataValues.name === "EVERYONE" || (!req.body.isExperienced && trainingProgram.dataValues.name === "OPTIONAL")) {
+            if (trainingProgram.CourseType.name === req.body.userType || trainingProgram.CourseType.name === 'EVERYONE' || (!req.body.isExperienced && trainingProgram.CourseType.name === 'OPTIONAL')) {
                 resData.push(trainingProgram);
             }
             else {
                 var resDataCourse = [];
-                var a;
+                var checkForEnrolledCourses;
                 trainingProgram.Courses.forEach(course => {
                     course.Classes.forEach(classes => {
                         classes.ClassRecords.forEach(classRecord => {
                             if (classRecord.User.email === req.body.email) {
-                                a = 1;
+                                checkForEnrolledCourses = 1;
                                 resDataCourse.push(course);
                             }
                         });
                     });
                 });
-                if (a === 1) {
+                if (checkForEnrolledCourses === 1) {
                     resData.push({
                         id: trainingProgram.id,
                         name: trainingProgram.name,
@@ -86,72 +86,5 @@ router.post('/getRequestOpenCourse', function (req, res) {
         res.send(datasend);
     });
 });
-
-router.post('/getMyCourses', function (req, res) {
-    var query = {
-        include: [{
-            model: models.TrainingProgram,
-            include: [{
-                model: models.Course,
-                include: [{
-                    model: models.Class,
-                    include: [{
-                        model: models.ClassRecord,
-                        include: [{
-                            model: models.User
-                        }]
-                    }]
-                }]
-            }]
-        }]
-    };
-
-    models.CourseType.findAll(query).then(function (myCourseTypes) {
-        var resMyCourses = [];
-        myCourseTypes.forEach(courseType => {
-            if (courseType.name === req.body.userType || courseType.name === 'EVERYONE' || (!req.body.isExperienced && courseType.name === 'OPTIONAL')) {
-                resMyCourses.push(courseType);
-            }
-            else {
-                var resDataCourse = [];
-                courseType.TrainingPrograms.forEach(trainingProgram => {
-                    trainingProgram.Courses.forEach(course => {
-                        course.Classes.forEach(cl => {
-                            cl.ClassRecords.forEach(classRecord => {
-                                if (classRecord.User.email === req.body.email) {
-                                    resDataCourse.push(course);
-                                    resData.push({
-                                        id: trainingProgram.id,
-                                        name: trainingProgram.name,
-                                        description: trainingProgram.description,
-                                        imgLink: trainingProgram.imgLink,
-                                        courseTypeId: trainingProgram.courseTypeId,
-                                        CourseType: trainingProgram.CourseType,
-                                        Courses: resDataCourse
-                                    });
-                                }
-                            });
-                        });
-                    });
-                });
-
-            }
-        })
-        var datasend = {
-            success: true,
-            msg: 'send list success',
-            myCourseTypes: resMyCourses,
-        };
-        res.send(datasend);
-    });
-
-
-});
-
-
-
-
-
-
 
 module.exports = router;
