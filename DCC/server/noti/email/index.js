72b3012afb75ef75d08d7bd3f5aad9d104c35e48
Tran Hoang Nam <Tran Hoang Nam>
2017-03-13 17:01:38 +0700

    [ Test case ] fix

--- DCC/server/noti/email/index.js
+++ DCC/server/noti/email/index.js
@@ -1,13 +1,10 @@
 var email_config = require('./email_config.json');
 var request = require('request');
 var email = {
-<<<<<<< HEAD
+
     send: function (receivers, subject, content, callback) {
-=======
-    send: function (receivers, subject, content) {
 
         const nodemailer = require('nodemailer');
->>>>>>> 27c9737945e157614d87e98d3e3fc123c42535ca
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -27,6 +24,7 @@ var email = {
         };
         transporter.sendMail(mailOptions, (error, info) => {
             callback(error, info);
-        }
+        });
+    }
 }
 module.exports = email.send;
