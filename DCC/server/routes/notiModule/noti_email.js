00108994c0c0e1b228bf6d7318ea32a852568180
Tran Hoang Nam <Tran Hoang Nam>
2017-03-15 13:43:59 +0700

    [ Try to fix test case ]

--- DCC/server/routes/notiModule/noti_email.js
+++ DCC/server/routes/notiModule/noti_email.js
@@ -7,12 +7,12 @@ router.post('/noti_email', function (req, res) {
     var subject = req.body.subject;
     var content = req.body.content;
     var listOfReceiver = req.body.listOfReceiver;
-    noti.email(listOfReceiver, subject, content);
     var datasend = {
         success: true,
         msg: 'sent email successfully'
     };
     res.send(datasend);
+    noti.email(listOfReceiver, subject, content);
 });
 
 module.exports = router;
