7b7f00eac05a16187c3dc2c7d27c6175fc5325b5
Tran Hoang Nam <Tran Hoang Nam>
2017-03-15 13:31:48 +0700

    [ Re-push ]

--- DCC/server/routes/notiModule/noti_email.js
+++ DCC/server/routes/notiModule/noti_email.js
@@ -8,7 +8,7 @@ router.post('/noti_email', function (req, res) {
     var content = req.body.content;
     var listOfReceiver = req.body.listOfReceiver;
     noti.email(listOfReceiver, subject, content);
-    datasend = {
+    var datasend = {
         success: true,
         msg: 'sent email successfully'
     };
