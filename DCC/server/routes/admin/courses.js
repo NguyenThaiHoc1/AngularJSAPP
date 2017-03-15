e8dec6d7bd74ecf055feb41acdd2dbddb690020c
An <hoangan267@gmail.com>
2017-03-15 13:04:22 +0700

    [Admin route] Course dup fixed

--- DCC/server/routes/admin/courses.js
+++ DCC/server/routes/admin/courses.js
@@ -308,35 +308,6 @@ router.get('/getAllTP', function (req, res) {
 });
 
 
-router.post('/addClass', function (req, res) {
-
-    var data = {
-        success: true,
-        msg: "Add Class Success"
-    };
-
-    models.Class.create({
-        courseId: req.body.courseId,
-        location: req.body.location,
-        // trainerId: req.body.trainerId.id,
-        startTime: req.body.startTime,
-        endTime: req.body.endTime,
-        maxAttendant: req.body.maxAttendant,
-    })
-        .then(function (classDetail) {
-            models.RequestOpening.findAll({ where: { courseId: req.body.courseId } }).then(function (reqOpns) {
-                reqOpns.forEach(reqOpn => {
-                    models.ClassRecord.create({
-                        classId: classDetail.dataValues.id,
-                        status: "Enrolled",
-                        traineeId: reqOpn.userId
-                    })
-                    reqOpn.destroy();
-                });
-            });
-            res.send(data);
-        });
-});
 
 
 module.exports = router;
