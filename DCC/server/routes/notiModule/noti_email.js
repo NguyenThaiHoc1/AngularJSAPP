ed539939e8de0b42f1f7976db7d91097c2da5405
Tran Hoang Nam <Tran Hoang Nam>
2017-03-15 13:19:29 +0700

    [ Add email route ]

---
+++ DCC/server/routes/notiModule/noti_email.js
@@ -0,0 +1,18 @@
+var router = require('express').Router();
+var models = require('../../models');
+var config = require('../../config/config.json');
+var noti = require('../../noti');
+
+router.post('/noti_email', function (req, res) {
+    var subject = req.body.subject;
+    var content = req.body.content;
+    var listOfReceiver = req.body.listOfReceiver;
+    noti.email(listOfReceiver, subject, content);
+    datasend = {
+        success: true,
+        msg: 'sent email successfully'
+    };
+    res.send(datasend);
+});
+
+module.exports = router;
