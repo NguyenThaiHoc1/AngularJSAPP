4c7d1a4b1e14815057632e64cbd787f006ddca2b
An <hoangan267@gmail.com>
2017-03-16 13:28:16 +0700

    [Notifications] Object Model added

---
+++ DCC/server/models/Notifications.js
@@ -0,0 +1,15 @@
+var _notificationsModel = require("./DataObjects/Notifications");
+var Sequelize = require('sequelize');
+var models = require("./index");
+
+module.exports = function (sequelize) {
+    var Notifications = sequelize.define('Notifications', _notificationsModel, {
+        charset: 'utf8',
+        collate: 'utf8_unicode_ci',
+        classMethods: {
+        },
+        tableName: 'notification',
+        timestamps: false
+    });
+    return Notifications;
+};
