def01443d844129358bf20f20ad66fd5104ecb44
huylam95 <13520364@gm.uit.edu.vn>
2017-03-16 10:03:33 +0700

    [Test case] Fix test case noti_email

--- DCC/server/routes/notiModule/noti_email.js
+++ DCC/server/routes/notiModule/noti_email.js
@@ -7,16 +7,15 @@ router.post('/noti_email', function (req, res) {
     var subject = req.body.subject;
     var content = req.body.content;
     var listOfReceiver = req.body.listOfReceiver;
+    var datasend = {
+        success: true,
+        msg: 'sent email successfully',
+    };
 
     noti.email(listOfReceiver, subject, content, function (err, data) {
         if (err) return console.error(err);
-        var datasend = {
-            success: true,
-            msg: 'sent email successfully',
-            data: data
-        };
-        res.send(datasend);
     });
+    res.send(datasend);
 
 });
 
