0896cbee27883d359430f76aaa6123a4cf4abefc
Ly Hon Quang <lyhonquang@gmail.com>
2017-03-17 10:32:11 +0700

    [Notification]server route

--- DCC/server/routes/notification/notification.js
+++ DCC/server/routes/notification/notification.js
@@ -2,12 +2,22 @@ var router = require('express').Router();
 var models = require('../../models');
 
 router.post('/getNotifications', function(req, res) {
-    models.Notifications.getAllNotificationByEmail(req.body.email, function(notifications) {
+    models.Notifications.getAllNotificationByEmail(req.body.email, notifications => {
         res.send({
             data: notifications,
             success: true,
-            msg: 'get ' + notifications.length + 'notifications'
+            msg: 'got ' + notifications.length + ' notifications'
         });        
+    });
+});
+
+router.post('/getNewNotifications', function(req, res) {
+    models.Notifications.getNewNotificationByEmail(req.body.email, notifications => {
+        res.send({
+            data: notifications,
+            success: true,
+            msg: 'got ' + notifications.length + ' notifications'
+        });
     });
 });
 
