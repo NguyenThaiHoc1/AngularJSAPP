92cafa7633628b490927d0779989ca7598ed4e74
Bành Quốc Danh <banhquocdanh.2011@gmail.com>
2017-03-17 16:55:55 +0700

    [CleanCode] push notification desktop

--- DCC/server/routes/user/userProfile.js
+++ DCC/server/routes/user/userProfile.js
@@ -61,7 +61,6 @@ router.post('/getUserInfo', function (req, res) {
 
 router.post('/updateUserProfile', function (req, res) {
     log.info('/routes/users: Save edit userprofile');
-    notification.desktop(["bqd@gmail.com"], 'update profile', 'msg');
     models.User.update(
         {
             username: req.body.username,
