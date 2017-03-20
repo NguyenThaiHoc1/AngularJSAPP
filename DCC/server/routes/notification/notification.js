0e852bae4cc24883e7845bdb7dbcd5fa21bd56a2
Bành Quốc Danh <banhquocdanh.2011@gmail.com>
2017-03-20 12:17:59 +0700

    [update NewNotification] new to old

--- DCC/server/routes/notification/notification.js
+++ DCC/server/routes/notification/notification.js
@@ -1,18 +1,30 @@
 var router = require('express').Router();
 var models = require('../../models');
 
-router.post('/getNotifications', function(req, res) {
+router.post('/getNotifications', function (req, res) {
     models.Notifications.getAllNotificationByEmail(req.body.email, notifications => {
         res.send({
             data: notifications,
             success: true,
             msg: 'got ' + notifications.length + ' notifications'
-        });        
+        });
     });
+    models.Notifications.update(
+        {
+            status: 0
+        },
+        {
+            where: {
+                email: req.body.email,
+                status: 1
+            }
+        }
+    );
+
 });
 
-router.post('/getNewNotifications', function(req, res) {
-    models.Notifications.getNewNotificationByEmail(req.body.email, notifications => {
+router.post('/getNotificationsNewNumber', function (req, res) {
+    models.Notifications.getNotificationsNewNumber(req.body.email, notifications => {
         res.send({
             data: notifications,
             success: true,
