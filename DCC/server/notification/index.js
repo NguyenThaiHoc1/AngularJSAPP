c5dff30cf2a7ebd5de24efb87b241cdbe332f3e4
Bành Quốc Danh <banhquocdanh.2011@gmail.com>
2017-03-21 18:18:39 +0700

    [notification] combine into one funciton

--- DCC/server/notification/index.js
+++ DCC/server/notification/index.js
@@ -1,4 +1,49 @@
-module.exports= {
-    email: require('./email'),
-    desktop: require('./desktop')
+var email = require('./email');
+var desktop = require('./desktop');
+var models = require('../models');
+var arr_email = [];
+var arr_desktop = [];
+function init(receivers) {
+    for (var i = 0; i < receivers.length; i++) {
+        models.User.getUserByEmail(receivers[i], function (user) {
+            if (user.isNotificationDesktop === true)
+                desktop([user.email], notification.subject, notification.content);
+            //arr_desktop.push(user.email);
+
+            if (user.isNotificationEmail === true)
+                email([user.email], notification.subject, notification.content);
+            //arr_email.push(user.email);
+
+        });
+    }
 }
+
+
+function send(receivers, notification, callback) {
+    // var arr_email = [];
+    // var arr_desktop = [];
+    // var arr_sms = [];
+    for (var i = 0; i < receivers.length; i++) {
+        models.User.getUserByEmail(receivers[i], function (user) {
+            if (user.isNotificationDesktop === true)
+                desktop([user.email], notification.subject, notification.content);
+            //arr_desktop.push(user.email);
+
+            if (user.isNotificationEmail === true)
+                email([user.email], notification.subject, notification.content);
+            //arr_email.push(user.email);
+
+        });
+    }
+    // if (arr_email.length > 0)
+    //     email(arr_email, notification.subject, notification.content);
+    // if (arr_desktop.length > 0)
+    //     desktop(arr_desktop, notification.subject, notification.content);
+
+
+
+}
+
+
+
+module.exports = send;
