ebeaeb3c888cf69c54a3162ad3cef0b98fcc1301
Bành Quốc Danh <banhquocdanh.2011@gmail.com>
2017-03-22 15:56:47 +0700

    [notification] send notification

--- DCC/server/notification/index.js
+++ DCC/server/notification/index.js
@@ -1,46 +1,30 @@
 var email = require('./email');
 var desktop = require('./desktop');
 var models = require('../models');
-var arr_email = [];
-var arr_desktop = [];
-function init(receivers) {
-    for (var i = 0; i < receivers.length; i++) {
-        models.User.getUserByEmail(receivers[i], function (user) {
-            if (user.isNotificationDesktop === true)
-                desktop([user.email], notification.subject, notification.content);
-            //arr_desktop.push(user.email);
-
-            if (user.isNotificationEmail === true)
-                email([user.email], notification.subject, notification.content);
-            //arr_email.push(user.email);
-
-        });
-    }
-}
-
 
 function send(receivers, notification, callback) {
-    // var arr_email = [];
-    // var arr_desktop = [];
-    // var arr_sms = [];
-    for (var i = 0; i < receivers.length; i++) {
-        models.User.getUserByEmail(receivers[i], function (user) {
-            if (user.isNotificationDesktop === true)
-                desktop([user.email], notification.subject, notification.content);
-            //arr_desktop.push(user.email);
+    models.User.getAllUsers(listUser => {
+        var arr_email = [];
+        var arr_desktop = [];
+        hashTable = new Object();
+        for (i = 0; i < listUser.length; i++) {
+            hashTable[listUser[i].email] = {
+                isNotificationEmail: listUser[i].isNotificationEmail,
+                isNotificationDesktop: listUser[i].isNotificationDesktop
+            }
+        }
 
-            if (user.isNotificationEmail === true)
-                email([user.email], notification.subject, notification.content);
-            //arr_email.push(user.email);
-
-        });
-    }
-    // if (arr_email.length > 0)
-    //     email(arr_email, notification.subject, notification.content);
-    // if (arr_desktop.length > 0)
-    //     desktop(arr_desktop, notification.subject, notification.content);
-
-
+        for (i = 0; i < receivers.length; i++) {
+            if (hashTable[receivers[i]].isNotificationEmail === true) {
+                arr_email.push(receivers[i]);
+            }
+            if (hashTable[receivers[i]].isNotificationDesktop === true) {
+                arr_desktop.push(receivers[i]);
+            }
+        }
+        desktop(arr_desktop, notification.subject, notification.content);
+        email(arr_email, notification.subject, notification.content);
+    });
 
 }
 
