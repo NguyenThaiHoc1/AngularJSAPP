8a3abc9656d83a640586b6336730135574f0f8fe
Tran Hoang Nam <Tran Hoang Nam>
2017-03-23 14:10:29 +0700

    [ Minor Fix ] Notifications

--- DCC/client/partials/notification/notification.js
+++ DCC/client/partials/notification/notification.js
@@ -10,43 +10,40 @@ myApp.config(function ($stateProvider) {
     });
 });
 
-myApp.factory('NotificationService',['$http','$rootScope', function($http, $rootScope) {
+myApp.factory('NotificationService', ['$http', '$rootScope', function ($http, $rootScope) {
     var factoryDefinition = {
-        updateNotificationSetting: function() {
+        updateNotificationSetting: function () {
             return $http.post('/user/userProfile/updateUserProfile', $rootScope.userInfo).success(function (data) { return data; });
         },
-        getNotifications: function() {
-            return $http.post('/notification/notification/getNotifications', $rootScope.userInfo).success(function (data) { return data; });            
+        getNotifications: function () {
+            return $http.post('/notification/notification/getNotifications', $rootScope.userInfo).success(function (data) { return data; });
         },
-        UpdateNotificationStatus: function(notification) {
-            return $http.post('/notification/notification/updateNotificationStatus', notification).success(function (data) {return data; });
+        UpdateNotificationStatus: function (notification) {
+            return $http.post('/notification/notification/updateNotificationStatus', notification).success(function (data) { return data; });
         }
     }
     return factoryDefinition;
 }]);
 
-myApp.controller('NotiController', ['$scope', '$rootScope', 'NotificationService', function($scope, $rootScope, NotificationService) {
-    function convertDate(date)
-    {
+myApp.controller('NotiController', ['$scope', '$rootScope', 'NotificationService', function ($scope, $rootScope, NotificationService) {
+    function convertDate(date) {
         var hour = date.getHours();
         var minute = date.getMinutes();
-        return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear()
-        + " " + (hour % 12 || 12) + ":" + (minute < 10 ? '0' : '') + minute 
-        + ' ' + (hour >= 12 ? 'PM' : 'AM');
+        return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()
+            + " " + (hour % 12 || 12) + ":" + (minute < 10 ? '0' : '') + minute
+            + ' ' + (hour >= 12 ? 'PM' : 'AM');
     }
 
-    $scope.getNotificationsList = function(){
-        NotificationService.getNotifications().then(function(notifications) {
-            for (var i = 0, j = notifications.data.data.length - 1; i < j; i++, j--)
-            {
+    $scope.getNotificationsList = function () {
+        NotificationService.getNotifications().then(function (notifications) {
+            for (var i = 0, j = notifications.data.data.length - 1; i < j; i++ , j--) {
                 var temp = notifications.data.data[i];
                 notifications.data.data[i] = notifications.data.data[j];
                 notifications.data.data[j] = temp;
             }
 
-            for(var i = 0; i < notifications.data.data.length; i++)
-            {
-                var date  = new Date(notifications.data.data[i].time);
+            for (var i = 0; i < notifications.data.data.length; i++) {
+                var date = new Date(notifications.data.data[i].time);
                 notifications.data.data[i].time = convertDate(date);
             }
 
@@ -55,7 +52,7 @@ myApp.controller('NotiController', ['$scope', '$rootScope', 'NotificationService
         });
     };
 
-    $scope.SaveSetting = function() {
+    $scope.SaveSetting = function () {
         NotificationService.updateNotificationSetting().then(function (result) {
             if (result.data.success) {
                 $rootScope.ShowPopupMessage("Setting saved", "success");
@@ -66,11 +63,7 @@ myApp.controller('NotiController', ['$scope', '$rootScope', 'NotificationService
         });
     };
 
-    $scope.UpdateNotificationStatus = function(notification) {
-        NotificationService.UpdateNotificationStatus(notification).then(function (result) {
-            if (result.data.status != 0) {
-                $rootScope.ShowPopupMessage("Something went wrong", "error");
-            }
-        });
+    $scope.UpdateNotificationStatus = function (notification) {
+        NotificationService.UpdateNotificationStatus(notification);
     }
 }]);
