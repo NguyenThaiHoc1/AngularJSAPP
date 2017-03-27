2ceb294ecb476ec06bda5e0f6aa5bc15fa00c45a
Nguyen Thi Lan Anh <lananhnt@outlook.com.vn>
2017-03-27 15:18:29 +0700

    [package.json] Adding module

--- DCC/app.js
+++ DCC/app.js
@@ -51,6 +51,5 @@ var server = http.createServer(app);
 createServer(server);
 server.listen(app.get('port'), function () {
     console.log('Server started on port ' + app.get('port'));
-    console.log('Server started on ' + app.get('port'));
 });
 module.exports = server;
