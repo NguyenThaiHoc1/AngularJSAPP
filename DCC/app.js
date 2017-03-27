7fb094a05772c036442a628d0283605cf887d0dd
Nguyen Thi Lan Anh <lananhnt@outlook.com.vn>
2017-03-27 11:52:45 +0700

    Sonar Test

--- DCC/app.js
+++ DCC/app.js
@@ -51,5 +51,6 @@ var server = http.createServer(app);
 createServer(server);
 server.listen(app.get('port'), function () {
     console.log('Server started on port ' + app.get('port'));
+    console.log('Server started on ' + app.get('port'));
 });
 module.exports = server;
