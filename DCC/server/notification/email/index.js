caf21d384aa3d1a739a7d93d2a902fda941f2551
Ly Hon Quang <lyhonquang@gmail.com>
2017-04-10 17:47:16 +0700

    [Mail]Nodemailer change to SMTP

--- DCC/server/notification/email/index.js
+++ DCC/server/notification/email/index.js
@@ -5,7 +5,8 @@ var email = {
     send: function (receivers, subject, content) {
 
         const nodemailer = require('nodemailer');
-        var transporter = nodemailer.createTransport({
+        const smtpTransport = require('nodemailer-smtp-transport');
+        var transporter = nodemailer.createTransport(smtpTransport({
             service: 'gmail',
             auth: {
                 type: 'OAuth2',
@@ -15,7 +16,7 @@ var email = {
                 refreshToken: email_config.USER_REFRESH_TOKEN,
                 accessToken: email_config.USER_ACCESS_TOKEN,
             }
-        });
+        }));
         var mailOptions = {
             from: '"DEK Notification System" <dektech.dcc@gmail.com>',
             to: receivers.toString(),
