f2436520a976d28ec0c8e8c0340cf4a240ebb6a2
Bành Quốc Danh <banhquocdanh.2011@gmail.com>
2017-04-10 11:52:24 +0700

    correct setting

--- DCC/settings.js
+++ DCC/settings.js
@@ -3,7 +3,7 @@ var settings = {
     /**       
     / ---------- DATABASE CONFIG ------------
     */
-    databaseHost: "192.168.122.23", //127.0.0.1
+    databaseHost: "192.168.122.20", //127.0.0.1
     databasePort: 3306, //3306
     databaseUserName: "root", // root
     databasePassword: "dekvn@123321", // dekvn@123321 or root
@@ -12,7 +12,7 @@ var settings = {
     /**       
     *---------- LDAP SERVER CONFIG ------------
     */
-    LDAPurl: 'ldap://192.168.122.23:389', //192.168.122.23:389
+    LDAPurl: 'ldap://192.168.122.20:389', //192.168.122.23:389
 
     /**       
     *---------- LOG CONFIG ------------
@@ -35,8 +35,8 @@ var settings = {
         "USER_EMAIL": "dektech.dcc@gmail.com",
         "USER_CLIENT_ID": "985840088773-5s0rmeo36dd1v2h15h658c8flikvu63j.apps.googleusercontent.com",
         "USER_CLIENT_SECRET": "Zk2q9G6IDYLATjkLCB0-R9P3",
-        "USER_ACCESS_TOKEN": "ya29.GlsXBKtapW12VeEXUil-kRM3vpdq6dkmY7-J00vnYbDSoNZwO86BmpOZRJm-aqX1hJvhPEvl1oNY7uoTbvHxVDkkGSUYChdcJy9pJjH6EqVc1A0YXkqymV7E972_",
-        "USER_REFRESH_TOKEN": "1/boN86F0olFg8TbihDAJRPkeJWKuAWp7Ik3HSV0s3nXE"
+        "USER_ACCESS_TOKEN": "ya29.GlspBL5M5LoN9Pq137IoHkNhcwG8BoqRXO5mArcOme7rECTkYvUoSlhpbfyGvQOp9gXD6AF1rloYWmgCtvN3_oceB8SaYacLhyDDdZVe6if5FzDmeVdRTwf-IS51",
+        "USER_REFRESH_TOKEN": "1/tstn10cRC_UPiSHQ9oo5jKwpXDvkQZjjlLvHBkxPFxU"
     },
 
     /**       
