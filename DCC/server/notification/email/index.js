f3d8b9f895da759f8292751f7e0086e32c50e001
Ly Hon Quang <lyhonquang@gmail.com>
2017-04-11 09:40:57 +0700

    [Mail]fix syntax

--- DCC/server/notification/email/index.js
+++ DCC/server/notification/email/index.js
@@ -7,7 +7,7 @@ var email = {
         const nodemailer = require('nodemailer');
         const smtpTransport = require('nodemailer-smtp-transport');
         var transporter = nodemailer.createTransport(smtpTransport({
-            service: 'Gmail',
+            service: 'gmail',
             auth: {
                 type: 'OAuth2',
                 user: email_config.USER_EMAIL,
