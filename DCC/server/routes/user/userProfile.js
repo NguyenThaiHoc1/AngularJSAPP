e8980d7da84abeacc76019114c7941fad9cde5aa
An <hoangan267@gmail.com>
2017-04-13 12:02:31 +0700

    [add user] add console log

--- DCC/server/routes/user/userProfile.js
+++ DCC/server/routes/user/userProfile.js
@@ -171,7 +171,7 @@ router.post('/addUser', function (req, res) {
                     isExperienced: 0,
                     userType: req.body.userType,
                 }).then(function () {
-
+                    console.log("ready to send");
                     sendEmail([req.body.email], "Register - Account Information", "Your account has been registered as " + req.body.username + " using the email: " + req.body.email + " with the auto-generated password of: " + req.body.password + " . You must change your password the first time you login otherwise you won't be able to access other features.");
                     res.send({
                         success: true,
