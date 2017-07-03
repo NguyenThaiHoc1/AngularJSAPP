853825155cb2dcf5690830644abc92e8de832c0c
Nguyen Minh Quan <minhquan.nguyen0511@gmail.com>
2017-07-03 16:29:50 +0700

    Update config.js

--- DCC/server/config/config.js
+++ DCC/server/config/config.js
@@ -59,9 +59,9 @@ module.exports =
         // LDAP server config , port 636
         server: {
             url: settings.LDAPurl,
-            bindDn: 'cn=admin,dc=dcc,dc=com',
+            bindDn: 'cn=admin,dc=example,dc=com',
             bindCredentials: 'root',
-            searchBase: 'dc=dcc,dc=com',
+            searchBase: 'dc=example,dc=com',
             searchFilter: '(mail={{username}})',
             /*
              tlsOptions: {
