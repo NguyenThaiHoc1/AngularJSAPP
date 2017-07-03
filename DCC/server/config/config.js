59ca57ffc5e408e230debabe14487fc3d6e0b090
Nguyen Minh Quan <minhquan.nguyen0511@gmail.com>
2017-07-03 16:18:57 +0700

    Update config.js

--- DCC/server/config/config.js
+++ DCC/server/config/config.js
@@ -63,16 +63,16 @@ module.exports =
             bindCredentials: 'root',
             searchBase: 'dc=dcc,dc=com',
             searchFilter: '(mail={{username}})',
-            
+            /*
              tlsOptions: {
                 ca:[
                     fs.readFileSync('/etc/ldap/ssl/mycert.pem'),
                     ],   
             
             },
-            
+            */
         },
-        credentialsLookup: basicAuth,
+        //credentialsLookup: basicAuth,
         //Log config
         "log": require('simple-node-logger').createLogManager(logConfigOptions).createLogger()
     }
